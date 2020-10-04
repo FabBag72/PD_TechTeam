5a997cd471c28c8aeeeeded1a4ffaa665035a30c
Fabrizio Bagnara <f.bagnara@beckhoff.it>
2020-10-04 12:06:57 +0200

    RPC method calling sample (not working)

---
+++ TcHmiResponsiveNavigation/Function/CalculationMethod.js
@@ -0,0 +1,17 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.742.0/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+(function (TcHmi) {
+    var Functions;
+    (function (Functions) {
+        var TcHmiResponsiveNavigation;
+        (function (TcHmiResponsiveNavigation) {
+            function CalculationMethod(par1) {
+                TcHmi.Symbol.writeEx("%s%PLC1.MAIN.Base::Calculation%/s%", { in1: 1, in2: 3, operator: 1 }, function (data) {
+                    console.log(data);
+                });
+            }
+            TcHmiResponsiveNavigation.CalculationMethod = CalculationMethod;
+        })(TcHmiResponsiveNavigation = Functions.TcHmiResponsiveNavigation || (Functions.TcHmiResponsiveNavigation = {}));
+        Functions.registerFunctionEx('CalculationMethod', 'TcHmi.Functions.TcHmiResponsiveNavigation', TcHmiResponsiveNavigation.CalculationMethod);
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
