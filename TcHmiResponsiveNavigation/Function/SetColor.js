cba35f03ead97275218bdbbe896b69d3a5b4c765
Fabrizio Bagnara <f.bagnara@beckhoff.it>
2021-01-06 11:27:40 +0100

    Add Function Binding example to modify the bordercolor property via javascript function

---
+++ TcHmiResponsiveNavigation/Function/SetColor.js
@@ -0,0 +1,25 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.742.5/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var TcHmiResponsiveNavigation;
+        (function (TcHmiResponsiveNavigation) {
+            function SetColor(Control_Identifier, SetColorRed, SetColorBlack, SetColorBlue) {
+                if (SetColorRed) {
+                    document.getElementById(Control_Identifier).style.borderColor = "red";
+                } else {
+                    if (SetColorBlack) {
+                        document.getElementById(Control_Identifier).style.borderColor = "black";
+                    } else {
+                        if (SetColorBlue) {
+                            document.getElementById(Control_Identifier).style.borderColor = "blue";
+                        }
+                    }
+                }
+            }
+            TcHmiResponsiveNavigation.SetColor = SetColor;
+        })(TcHmiResponsiveNavigation = Functions.TcHmiResponsiveNavigation || (Functions.TcHmiResponsiveNavigation = {}));
+        Functions.registerFunctionEx('SetColor', 'TcHmi.Functions.TcHmiResponsiveNavigation', TcHmiResponsiveNavigation.SetColor);
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
